ff16f23bafdabc1f3145e4cf5d16b5436fd7f1d2
Yaroslav Demenskyi <ydemenskyi@singlestore.com>
2025-05-05 09:15:27 -0700

    add aws docker configs

--- apps/client/components/header.tsx
+++ apps/client/components/header.tsx
@@ -23,7 +23,7 @@ export function Header({ className, ...props }: HeaderProps) {
 
       <Button asChild>
         <Link
-          href="https://portal.singlestore.com/intention/cloud?utm_source=yaroslav&utm_medium=app&utm_campaign=general-technical&utm_content=real-time-analytics-on-operational-data"
+          href="https://portal.singlestore.com/intention/cloud?utm_source=yaroslav&utm_medium=app&utm_campaign=general-technical&utm_content=real-time-analytics-on-operational-data-mysql-vs-postgresql-vs-singlestore"
           target="_blank"
         >
           Try SingleStore Free
